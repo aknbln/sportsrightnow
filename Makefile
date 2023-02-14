73ee84ad831756c9074abd736c5eba2c927313cf
Johann Ramirez <johannramirez@Johanns-MacBook-Pro.local>
2023-02-14 15:46:04 -0600

    trying to add rebased content

---
+++ Makefile
@@ -0,0 +1,44 @@
+# get git config
+config:
+	git config -l
+
+# get git log
+SportsRightNow.log.txt:
+	git log > SportsRightNow.log.txt
+	
+clean:
+	rm -f *.tmp
+# docker for front end: runs website on local 3000 port
+frontend-docker:
+	docker run -dp 3000:3000 front-end
+# Docker for running the backend as a developer on local machine
+backend-dev-docker:
+	docker run --rm -it -p 5000:5000 SportsRightNow-backend-dev
+#build backend
+build-backend :
+	docker build -t SportsRightNow-backend-dev -f dev.Dockerfile .
+#build frontend
+build-frontend :
+	docker build -t front-end front-end/
+
+#run unit tests
+python-tests:
+	echo "Running python unit tests..."
+	python3 back-end/tests.py
+
+# get git status
+status:
+	make clean
+	@echo
+	git branch
+	git remote -v
+	git status
+
+# download files from the IDB code repo
+pull:
+	make clean
+	@echo
+	git pull
+	git status
+
+all:
---
+++ front-end/src/App.js
@@ -0,0 +1,56 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar'
+import {LinkContainer} from 'react-router-bootstrap'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { NavDropdown } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div>
+      <Router>
+
+      <Navbar bg='light'>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Container>
+          <Navbar.Brand>Sports Now</Navbar.Brand>
+            <Nav className='me-auto'>
+              <LinkContainer to = "/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to = "/About">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+
+              <NavDropdown title="Models" id="models-dropdown">
+                <LinkContainer to = "/Players">
+                  <NavDropdown.Item>Players</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to = "/Teams">
+                  <NavDropdown.Item>Teams</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to = "/Events">
+                  <NavDropdown.Item>Events</NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+            </Nav>
+        </Container>
+        </Navbar.Collapse>
+      </Navbar>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}        
+export default App;
