dcfe60003d5158d951efa350d2f7028338c805bd
Neepsy <eric55664@gmail.com>
2023-03-05 02:43:52 -0600

    Put test files in the right spot

--- front-end/selenium/SeleniumTests.py
+++ front-end/selenium/SeleniumTests.py
@@ -9,60 +9,111 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import sys
 
-class SeleniumTests():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_about(self):
-    self.driver.get("https://www.sportsrightnow.me/")
-    self.driver.find_element(By.LINK_TEXT, "About").click()
-    element = self.driver.find_element(By.LINK_TEXT, "About")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, ".container:nth-child(1) > h2")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".container:nth-child(1) > h2").text == "Our Team"
-    element = self.driver.find_element(By.CSS_SELECTOR, ".container:nth-child(2) > h2")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".container:nth-child(2) > h2").text == "Technologies Utilized"
-    element = self.driver.find_element(By.CSS_SELECTOR, ".container:nth-child(3) > h2")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".container:nth-child(3) > h2").text == "APIs Utilized"
 
-  def test_aboutRepolink(self):
-    self.driver.get("https://www.sportsrightnow.me/")
-    self.driver.find_element(By.LINK_TEXT, "About").click()
-    element = self.driver.find_element(By.LINK_TEXT, "Repo Link")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    elements = self.driver.find_elements(By.LINK_TEXT, "Repo Link")
-    assert len(elements) > 0
-    self.driver.find_element(By.LINK_TEXT, "Repo Link").click()
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".home-panel-title")
-    assert len(elements) > 0
+class TestColors:
+    OK = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    RESET = '\033[0m'
 
-  def test_notFound(self):
-    self.driver.get("https://www.sportsrightnow.me/")
-    self.driver.get("https://www.sportsrightnow.me/fakepage")
-    assert self.driver.find_element(By.CSS_SELECTOR, "h1").text == "404"
 
-  def test_notFoundHomeLink(self):
-    self.driver.get("https://www.sportsrightnow.me/")
-    self.driver.get("https://www.sportsrightnow.me/fakepage")
-    assert self.driver.find_element(By.CSS_SELECTOR, "h1").text == "404"
-    assert self.driver.find_element(By.LINK_TEXT, "Return Home").text == "Return Home"
-    self.driver.find_element(By.LINK_TEXT, "Return Home").click()
-    assert self.driver.find_element(By.CSS_SELECTOR, "h2").text == "Sports Now"
+class SeleniumTests:
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
 
-print("Hi")
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_about(self):
+        self.driver.get("https://www.sportsrightnow.me/")
+        self.driver.find_element(By.LINK_TEXT, "About").click()
+
+        team_header = self.driver.find_element(By.CSS_SELECTOR, ".container:nth-child(1) > h2")
+        driver.execute_script("arguments[0].scrollIntoView();", team_header)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of(team_header))
+        assert team_header.text == "Our Team"
+
+        tech_header = self.driver.find_element(By.CSS_SELECTOR, ".container:nth-child(2) > h2")
+        driver.execute_script("arguments[0].scrollIntoView();", tech_header)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of(tech_header))
+        assert tech_header.text == "Technologies Utilized"
+
+        api_header = self.driver.find_element(By.CSS_SELECTOR, ".container:nth-child(3) > h2")
+        self.driver.execute_script("arguments[0].scrollIntoView();", api_header)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of(api_header))
+        assert api_header.text == "APIs Utilized"
+
+    def test_about_repolink(self):
+        self.driver.get("https://www.sportsrightnow.me/")
+        self.driver.find_element(By.LINK_TEXT, "About").click()
+
+        link = self.driver.find_element(By.LINK_TEXT, "Repo Link")
+        self.driver.execute_script("arguments[0].scrollIntoView();", link)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of(link))
+        link.click()
+        url = self.driver.current_url
+        assert url == "https://gitlab.com/johannramirez07/cs373-idb-18"
+
+    def test_not_found(self):
+        self.driver.get("https://www.sportsrightnow.me/")
+        self.driver.get("https://www.sportsrightnow.me/fakepage")
+        assert self.driver.find_element(By.CSS_SELECTOR, "h1").text == "404"
+
+    def test_not_found_home(self):
+        self.driver.get("https://www.sportsrightnow.me/")
+        self.driver.get("https://www.sportsrightnow.me/fakepage")
+        assert self.driver.find_element(By.CSS_SELECTOR, "h1").text == "404"
+        assert self.driver.find_element(By.LINK_TEXT, "Return Home").text == "Return Home"
+        self.driver.find_element(By.LINK_TEXT, "Return Home").click()
+        assert self.driver.find_element(By.CSS_SELECTOR, "h2").text == "Sports Now"
+
+
+# returns 0 if okay, 1 if fail (error counter)
+def run_test(test, pass_msg, fail_msg):
+    try:
+        test()
+    except Exception as ex:
+        print(TestColors.FAIL + "✘ " + TestColors.RESET + fail_msg)
+        print(str(ex))
+        return 1
+    else:
+        print(TestColors.OK + "✔ " + TestColors.RESET + pass_msg)
+        return 0
+
+
+if sys.platform == "win32":
+    driver = webdriver.Chrome('./chromedriver-win.exe')
+elif sys.platform == "linux":
+    pass
+    # get linux webdriver
+else:
+    print("Unsupported OS! Use windows or linux")
+    exit(-1)
+
+selenium_tests = SeleniumTests()
+selenium_tests.driver = driver
+
+error_count = 0
+test_count = 0
+
+test_count += 1
+error_count += run_test(selenium_tests.test_not_found, "404 page OK", "404 page not working")
+
+test_count += 1
+error_count += run_test(selenium_tests.test_not_found_home, "404 page home link OK", "404 page home link not working")
+
+test_count += 1
+error_count += run_test(selenium_tests.test_about, "About page OK", "About page not working")
+
+test_count += 1
+error_count += run_test(selenium_tests.test_not_found, "About page repo link OK", "About page repo link not working")
+
+passed = test_count - error_count
+print()
+print(str(passed) + "/" + str(test_count) + " tests passed!")
+
+if error_count > 0:
+    exit(-1)
