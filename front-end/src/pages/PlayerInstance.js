70b521a601ce6a4cae1af09a966cf32f316dcf6b
Neepsy <eric55664@gmail.com>
2023-03-11 17:00:40 +0100

    PlayerInstance now uses API to get data

--- front-end/src/pages/PlayerInstance.js
+++ front-end/src/pages/PlayerInstance.js
@@ -2,40 +2,53 @@ import React, { useEffect, useState } from 'react'
 import { playerData } from '../assets/PlayerData'
 import {Link, useSearchParams} from 'react-router-dom'
 
+import axios from 'axios'
+
+const ax = axios.create({
+  baseURL: "http://api.sportsrightnow.me/players/"
+})
 
 const PlayersInstance = ({}) =>{
 
     const [searchParams, setSearchParams] = useSearchParams()
     const [playerId, setPlayerId] = useState(0)
-    const [currentData, setCurrentData] = useState([])
-    const teamString = '/teams/' + currentData.team_id
+    const [fetchedData, setFetchedData] = useState([])
 
     useEffect(() => {
       let id = searchParams.get("id")
       setPlayerId(id)
-      setCurrentData(playerData[id - 1])
+      const fetchData = async() => {
+        await ax
+        .get(id)
+        .then((response) => (console.log(response.data.data), setFetchedData(response.data.data)))
+      }
+
+      fetchData();
     }, [])
+
+    
 
     return (
         <div className="App">
             <header className="App-header">
-              <h1>{currentData.name}</h1>
+              <h1>{fetchedData.name}</h1>
             </header>
             <div className='App-body'>
                 <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
-                    <img src={currentData.image} style={{float: 'left', width:'25%'}} alt="logo" />
+                    <img src={fetchedData.headshot} style={{float: 'left', width:'25%'}} alt="picture" />
                     <div style={{width: "10%"}}/>
                     <p style={{float: 'right'}}>
-                        Team: {currentData.team}<br/>
-                        Age: {currentData.age} <br/>
-                        Nationality: {currentData.country} <br/>
-                        Height: {currentData.height}
+                        Team: {fetchedData.team} <br/>
+                        League: {fetchedData.league} <br/>
+                        Birthday: {fetchedData.birthdate} <br/>
+                        Height: {fetchedData.height} <br/>
+                        Weight: {fetchedData.weight} <br/>
+                        College: {fetchedData.college}
                     </p>
                 </div>
               <br/>
               <br/>
               <Link to='/players'>Back</Link>
-              <Link to={teamString}>{currentData.team}</Link>
             </div>
           </div>
     )
