1c43222140843147a5410536ddaa059d53fcacb4
Neepsy <eric55664@gmail.com>
2023-03-28 00:58:27 -0500

    Add additional backend unit tests

--- back-end/tests.py
+++ back-end/tests.py
@@ -35,6 +35,26 @@ class Tests(unittest.TestCase):
             data = response.json["data"]
             self.assertEqual(len(data), 4345)
 
+    def testGetPlayerById(self):
+        with self.client:
+            response = self.client.get("/players/1")
+            self.assertEqual(response.status_code, 200)
+            data = response.json["data"]
+            self.assertEqual(data["name"], "Caleb Houstan")
+
+    def testGetTeamById(self):
+        with self.client:
+            response = self.client.get("/teams/1")
+            self.assertEqual(response.status_code, 200)
+            data = response.json["data"]
+            self.assertEqual(data["name"], "Boston Celtics")
+
+    def testGetEventById(self):
+        with self.client:
+            response = self.client.get("/events/1")
+            self.assertEqual(response.status_code, 200)
+            data = response.json["data"]
+            self.assertEqual(data["name"], "Golden State Warriorss vs. Phoenix Suns")
 
 if __name__ == "__main__":
     unittest.main()
